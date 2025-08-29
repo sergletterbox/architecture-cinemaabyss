233420395d69aacfcfe38c44fa74b440e8ef62ac
Sergey <sergletterbox@gmail.com>
2025-08-29 17:54:05 +0300

    kuber setup

--- src/microservices/proxy/src/main/java/com/smarthome/temperatureapi/controller/HealthController.java
+++ src/microservices/proxy/src/main/java/com/smarthome/temperatureapi/controller/HealthController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -18,5 +19,17 @@ public class HealthController {
                 "service", "Proxy",
                 "version", "1.0.0"
         ));
+    }
+
+    @GetMapping("/ready")
+    public ResponseEntity<Map<String, Object>> readinessCheck() {
+        Map<String, Object> status = new HashMap<>();
+        boolean isReady = true;
+
+
+        status.put("status", isReady ? "READY" : "NOT_READY");
+        status.put("timestamp", System.currentTimeMillis());
+
+        return ResponseEntity.ok(status);
     }
 }
